e72ec31c1a101f8bdc22c0a455f17dc295e1a626
Junwon Lee <cpprhtn@naver.com>
2022-07-06 20:12:11 +0900

    상하좌우

---
+++ Implementation/04/01/1_cpprhtn.c
@@ -0,0 +1,37 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int n;
+char str[100];
+int x = 1, y = 1;
+
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { -1, 1, 0, 0 };
+char moveTypes[4] = { 'L', 'R', 'U', 'D' };
+
+int main(void) {
+    scanf(" %d ", &n);
+    gets_s(str);
+
+    for (int i = 0; i < strlen(str); i++) {
+        char plan = str[i];
+
+        int nx = -1, ny = -1;
+        for (int j = 0; j < 4; j++) {
+            if (plan == moveTypes[j]) {
+                nx = x + dx[j];
+                ny = y + dy[j];
+            }
+        }
+
+        if (nx < 1 || ny < 1 || nx > n || ny > n) 
+            continue;
+
+        x = nx;
+        y = ny;
+    }
+    printf("%d %d", x, y);
+    return 0;
+}
